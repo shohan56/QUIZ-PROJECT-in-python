789c8fd62a2736a1007e0015c1ab0168e6496eb4
shohan56 <83160687+shohan56@users.noreply.github.com>
2024-04-25 16:08:37 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+print (" wellcome to my computer quiz")
+
+playing = input (" do you want to play? ")
+if playing != "yes":
+    quit()
+print("okay lets play:")
+answer = input(" what is your name? ")
+if answer == "MD ABU SAYED":
+ print("correct!")
+else:
+    print("incorrect ")
+answer = input("where are you come form? ")
+if answer == " Kushtia":
+ print("correct!")
+else:
+    print("incorrect ")
